5b2a3c3531df0d979793fadc488af988fa6131f9
BodnarAgnes <bodnar.agnes@asdfinformatika.hu>
2017-08-16 12:34:03 +0200

    10. óra feladatai

---
+++ 10-2.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>10. óra 1. feladat</title>
+  <meta name="author" content="">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link href="css/normalize.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <h1>Egyszerű számológép</h1>
+
+  <?php if( isset( $_POST['op1'] ) && isset( $_POST['op3'] ) ){
+
+    echo "<h2>" . $_POST['szamlalo'] . ". Eredmény</h2>";
+
+    if( $_POST['op1'] === "" || $_POST['op3'] === "" ){
+      echo "<h2>Hiányzó adat! Próbáld újra.</h2>";
+    }else{
+
+      switch( $_POST['op2'] ){
+        case "+":
+          $eredmeny = intval( $_POST['op1'] ) + intval( $_POST['op3'] );
+          echo "<p>" . $_POST['op1'] . " + " . $_POST['op3'] . " = " . $eredmeny . "</p>";
+          break;
+        case "-":
+          $eredmeny = intval( $_POST['op1'] ) - intval( $_POST['op3'] );
+          echo "<p>" . $_POST['op1'] . " - " . $_POST['op3'] . " = " . $eredmeny . "</p>";
+          break;
+        case "*":
+          $eredmeny = intval( $_POST['op1'] ) * intval( $_POST['op3'] );
+          echo "<p>" . $_POST['op1'] . " * " . $_POST['op3'] . " = " . $eredmeny . "</p>";
+          break;
+        case "/":
+          $eredmeny = intval( $_POST['op1'] ) / intval( $_POST['op3'] );
+          echo "<p>" . $_POST['op1'] . " / " . $_POST['op3'] . " = " . $eredmeny . "</p>";
+          break;
+      }
+    }
+  ?>
+
+  <form action="<?php print $_SERVER['PHP_SELF'] ?>" method="post">
+    <input type="hidden" name="szamlalo" id="szamlalo" value="<?php echo ( intval( $_POST['szamlalo'] ) + 1 ) ; ?>" />
+    <input type="submit" value="Új művelet" />
+  </form>
+
+  <?php }else{ ?>
+
+    <form action="<?php print $_SERVER['PHP_SELF'] ?>" method="post">
+
+      <label for="op1">
+        Első operandus:
+      </label>
+      <input type="text" name="op1" id="op1" <?php if( isset($_POST['op1']) ){ echo 'value="' . $_POST['op1'] . '"'; } ?>/>
+
+      <label for="op2">
+        Operátor:
+      </label>
+      <select name="op2" id="op2" <?php if( isset($_POST['op2']) ){ echo "value=\"" . $_POST['op2'] . "\""; } ?>>
+        <option value="+">Összeadás</option>
+        <option value="-">Kivonás</option>
+        <option value="*">Szorzás</option>
+        <option value="/">Osztás</option>
+      </select>
+
+      <label for="op3">
+        Második operandus:
+      </label>
+      <input type="text" name="op3" id="op3" <?php if( isset($_POST['op3']) ){ echo "value=\"" . $_POST['op3'] . "\""; } ?>/>
+
+      <input type="hidden" name="szamlalo" id="szamlalo" value="<?php echo ( isset( $_POST['szamlalo'] ) ? $_POST['szamlalo'] : 1 ) ?>" />
+
+      <input type="submit" value="Számol!" />
+
+    </form>
+
+  <?php } ?>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="js/script.js"></script>
+</body>
+
+</html>
