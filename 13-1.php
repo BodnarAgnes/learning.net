cef12c6355d89e898b5564dd9616887335fc021a
BodnarAgnes <bodnar.agnes@asdfinformatika.hu>
2017-08-21 16:53:02 +0200

    13. lesson 2 in 1 finished

--- 13-1.php
+++ 13-1.php
@@ -15,6 +15,7 @@
 <body>
 
   <h1>Üzenőfal</h1>
+  <h2>Szólj hozzá!</h2>
   <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
     <label for="email">E-mail cím* (max. 70 karakter)</label>
     <br/>
@@ -26,20 +27,76 @@
     <br/>
     <input type="submit" value="Küldés"/>
   </form>
+  <h2>Hozzászólások</h2>
 
   <?php
 
   require_once("DB.php");
 
   $user = "root";
-  
+  $pass = "";
+  $host ="localhost";
+  $database = "13egy";
 
-  $dsn = "sqlite://./adatbazis.db";
+
+  $dsn = "mysql://$user:$pass@$host/$database";
   $db = DB::connect( $dsn );
 
   if( DB::isError( $db ) ){
     die( $db->getMessage() );
   }
+
+  if( DB::isError( $db->query( "SELECT * FROM uzenetek" ) ) ){
+    $create_table = $db->query("CREATE TABLE uzenetek (id INTEGER PRIMARY KEY AUTO_INCREMENT, email VARCHAR(70), message VARCHAR(255), created TIMESTAMP)");
+  }
+
+  $uzenetek = $db->query( "SELECT * FROM uzenetek" );
+
+  ?>
+  <table>
+
+  <?php
+
+  while($uzenet = $uzenetek->fetchRow( DB_FETCHMODE_ASSOC ) ){
+
+    $flood = FALSE;
+
+    if( @$_POST['message'] === $uzenet['message'] ){
+      $flood = TRUE;
+    }
+
+    ?>
+    <tr>
+      <td>
+        <?php echo $uzenet['email'] ?>
+      </td>
+      <td>
+        <?php echo $uzenet['message'] ?>
+      </td>
+    </tr>
+  <?php
+  }
+  ?>
+  </table>
+  <?php
+
+  if( !empty($_POST['email']) && !empty($_POST['message']) ){
+
+    if( !$flood ){
+
+      $new_message = array(
+        "id"      => $db->nextID('uzenet'),
+        "email"   => $_POST['email'],
+        "message" => $_POST['message']
+      );
+
+      $db->autoExecute('uzenetek', $new_message, DB_AUTOQUERY_INSERT );
+
+    }
+  }
+
+  @$max->free();
+  $db->disconnect();
 
   ?>
 
